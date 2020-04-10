3c0095974aff6aebf432ccc0ffd3274c2ab62683
naimo84 <git@neumann-benjamin.de>
2020-04-10 18:02:30 +0200

    refactor: moved getConfig out

--- src/lib.ts
+++ src/lib.ts
@@ -53,26 +53,7 @@ export default class KalenderEvents {
         }
     }
 
-    getConfig(config: Config, node: any, msg: any): Config {
-        return {
-            url: msg?.url || config?.url,
-            language: msg?.language || config?.language,
-            replacedates: msg?.replacedates || config?.replacedates,
-            caldav: msg?.caldav || config?.caldav,
-            username: msg?.username || config?.username,
-            password: msg?.password || config?.password,
-            calendar: msg?.calendar || config?.calendar,
-            filter: msg?.filter || node.filter,
-            trigger: msg?.trigger || node.trigger || 'always',
-            preview: parseInt(msg?.preview || node?.preview || node?.endpreview || 10),
-            previewUnits: msg?.previewUnits || node?.previewUnits || node?.endpreviewUnits || 'd',
-            pastview: parseInt(msg?.pastview || node?.pastview || 0),
-            pastviewUnits: msg?.pastviewUnits || node?.pastviewUnits || 'd',
-            offset: parseInt(msg?.offset || node?.offset || 0),
-            offsetUnits: msg?.offsetUnits || node?.offsetUnits || 'm',
-            rejectUnauthorized: msg?.rejectUnauthorized || node?.rejectUnauthorized || false
-        } as Config;
-    }
+    
 
     convertEvents(events: any): any[] {
         let retEntries: any = [];
