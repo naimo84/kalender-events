6db9fbb3472b436d89085b161091f305cf25ef6c
naimo84 <git@naimo84.dev>
2020-05-01 09:04:50 +0200

    refactor: moved types

--- types/index.d.ts
+++ types/index.d.ts
@@ -4,3 +4,4 @@ import { KalenderEvents } from '../src/lib';
 export * from '../src/lib';
 export * from '../src/config';
 export default KalenderEvents;
+export { KalenderEvents as KalenderEvents };
