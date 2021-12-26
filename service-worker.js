aac55f57345b06bfd1a34207a1553da6d8d8e028
naimo84 <git@naimo84.dev>
2021-12-26 11:41:33 +0000

    Deploying to gh-pages from @ naimo84/kalender-events@5ceec58c77fda294fe742898430fed98eeae166e 🚀

--- service-worker.js
+++ service-worker.js
@@ -27,7 +27,7 @@ self.addEventListener('message', (event) => {
 self.__precacheManifest = [
   {
     "url": "404.html",
-    "revision": "f7890771c6ecbe45e8709ccd6973c7ed"
+    "revision": "2db08193f4fe12912ada1e5e063ef594"
   },
   {
     "url": "assets/css/0.styles.c95ac92f.css",
@@ -38,44 +38,68 @@ self.__precacheManifest = [
     "revision": "83621669651b9a3d4bf64d1a670ad856"
   },
   {
-    "url": "assets/js/10.0f6e5c34.js",
-    "revision": "4e1c9f83c7cc8a63f03f40b7564d6c86"
+    "url": "assets/js/10.78c34079.js",
+    "revision": "55718ba835312ba2da4016f9b67c0574"
   },
   {
-    "url": "assets/js/11.4513b961.js",
-    "revision": "af8ce31dde230af5cc98fbd9a0f3c4c5"
+    "url": "assets/js/11.192f1b83.js",
+    "revision": "dceb60ca84f69520ab5638f656b2b9d6"
   },
   {
-    "url": "assets/js/12.ad980a0a.js",
-    "revision": "b261d360c1b100d0c982a7a8c411d5de"
+    "url": "assets/js/12.1151e324.js",
+    "revision": "9042fe8fdfa87b86c0506844a03b0d4d"
   },
   {
-    "url": "assets/js/13.6f837984.js",
-    "revision": "43b56e35818e31a16df7b86ece9b1c18"
+    "url": "assets/js/13.7fe977ac.js",
+    "revision": "c19766e44f783effb79bbf3199e0eaa9"
   },
   {
-    "url": "assets/js/14.481e1109.js",
-    "revision": "505a90a55b13362c54808321708ee10f"
+    "url": "assets/js/14.8a0a559f.js",
+    "revision": "d8797f02d4e24ba0f436633af7c93e65"
   },
   {
-    "url": "assets/js/3.5d8eef78.js",
-    "revision": "9fc7c74255cc493924a63f69d1a9e2b3"
+    "url": "assets/js/15.6aecef19.js",
+    "revision": "a25227ac00a0367e92e4caa0b2bf089d"
   },
   {
-    "url": "assets/js/4.e8548c46.js",
-    "revision": "2ff3d0dc569fd15d2864980deecefd81"
+    "url": "assets/js/16.9d76b840.js",
+    "revision": "a3df3c9d06190a8f2de93c4b98bb4ee2"
   },
   {
-    "url": "assets/js/5.33dac6e7.js",
-    "revision": "4020d982a6c4605a128263170c7fd723"
+    "url": "assets/js/17.6351cfb0.js",
+    "revision": "c484a922d891fb7fe4770cac3481f5eb"
+  },
+  {
+    "url": "assets/js/18.8ef203c2.js",
+    "revision": "629a4425b68c88f2ea5f6903ded90f2c"
+  },
+  {
+    "url": "assets/js/19.192227a0.js",
+    "revision": "7022b7b4480a5aa6d7053e7541b1f9b9"
+  },
+  {
+    "url": "assets/js/20.1c434b64.js",
+    "revision": "9130bc7d7279ff2713ae3811343700fe"
+  },
+  {
+    "url": "assets/js/3.34aa6079.js",
+    "revision": "da20c3b610fbbabd4b752702d9b01403"
+  },
+  {
+    "url": "assets/js/4.1761f527.js",
+    "revision": "40f24a8fa2de918ba4c76d0554162e47"
+  },
+  {
+    "url": "assets/js/5.33ba41d4.js",
+    "revision": "d99b52a076aaeccd3db37eb49dc1b42b"
   },
   {
     "url": "assets/js/6.81c05968.js",
     "revision": "39538e2df6bdfbd91cee9751bc71f737"
   },
   {
-    "url": "assets/js/7.dd294fc7.js",
-    "revision": "d264c527b153c7f2c643dbfa2040b6da"
+    "url": "assets/js/7.8f74bc03.js",
+    "revision": "d926ba304d5a4d3b1b9a6607e4e61fe2"
   },
   {
     "url": "assets/js/8.4d67ef36.js",
@@ -86,8 +110,8 @@ self.__precacheManifest = [
     "revision": "253e60984da98fb551670be349471793"
   },
   {
-    "url": "assets/js/app.b50e8af1.js",
-    "revision": "1ced729e8d8371762f1605cbc7b672d9"
+    "url": "assets/js/app.a56f6db3.js",
+    "revision": "d3a86b4878006f71b831fd8dc495e5fd"
   },
   {
     "url": "assets/js/vendors~flowchart.4bff301a.js",
@@ -95,7 +119,19 @@ self.__precacheManifest = [
   },
   {
     "url": "config/index.html",
-    "revision": "3d0f37583bd16d6f3eb9bc37650d04c2"
+    "revision": "11a06d4d5a5b3d5371fdb2f8c7320477"
+  },
+  {
+    "url": "examples/cli.html",
+    "revision": "0422aa8472adf650bb20315e3c4cd701"
+  },
+  {
+    "url": "examples/index.html",
+    "revision": "76a61aa4ea895e04721d9bf32bf8f473"
+  },
+  {
+    "url": "examples/lib.html",
+    "revision": "c9870300bab74618b80777e5bf88db3c"
   },
   {
     "url": "favicon.png",
@@ -107,15 +143,27 @@ self.__precacheManifest = [
   },
   {
     "url": "guide/debug.html",
-    "revision": "f9bf2dc01087f985eb4ef0654d0a22bf"
+    "revision": "1c6d7cc10612ff4619e1695e0eb24577"
+  },
+  {
+    "url": "guide/examples_cli.html",
+    "revision": "d5483589f734716b16657d5f9e05d952"
+  },
+  {
+    "url": "guide/examples_lib.html",
+    "revision": "7113f6bd9b25e7ccc73089f8d783357e"
+  },
+  {
+    "url": "guide/examples.html",
+    "revision": "e4280741107cdf9392386df21cefdc0c"
   },
   {
     "url": "guide/icloudsecure.html",
-    "revision": "9190353ff3e58eee2ec89f31da64d493"
+    "revision": "64e1b7b6e404269511118fdbdf1d3925"
   },
   {
     "url": "guide/index.html",
-    "revision": "049ba9c44dd851c88d90e3cb75a80739"
+    "revision": "7339b4d3023cd1f62c8f5c5a99e1e20e"
   },
   {
     "url": "icons/android-chrome-192x192.png",
@@ -179,7 +227,7 @@ self.__precacheManifest = [
   },
   {
     "url": "index.html",
-    "revision": "e5d2e83ae891cb25fd2da41810335d3f"
+    "revision": "7ebc00822dac6c084ab30b73c876fe7b"
   }
 ].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
